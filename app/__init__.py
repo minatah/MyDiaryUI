80873ecc18fe397b3c68cce5e051b47fe4488825
minatah <namiroaminah@gmail.com>
2018-07-26 11:27:54 +0300

    challenge two

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_restful import Api
+
+from app.views import Entries, SingleEntry
+
+app = Flask(__name__)
+app.secret_key = "aminah"
+
+"""Am initializing an API for my application"""
+api = Api(app)
+
+api.add_resource(Entries,'/api/v1/entries')
+api.add_resource(SingleEntry,'/api/v1/entries/<entryId>')
---
+++ app/models.py
@@ -0,0 +1,20 @@
+
+import json
+
+
+class EntryModel:
+    """create a constructor"""
+    def __init__(self, id, title, content, date):
+        self.id = id
+        self.title_ = title
+        self.content_ = content
+        self.date_ = date
+
+    def myjson(self):
+        return json.dumps({
+            'id': self.id,
+            'title': self.title_,
+            'content': self.content_,
+            'date': self.date_
+
+        })
